1b1127487beef64d45fbb414f0b2272cf33c2e39
npwyudi16 <nurprayugowahyudi@gmail.com>
2022-09-01 23:02:40 +0700

    awalan

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_coffe/screen/home_screen.dart';
+import 'package:flutter_coffe/screen/landing_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SB Coffe',
+      debugShowCheckedModeBanner: false,
+      initialRoute: LandingScreen.namedRoute,
+      routes: {
+        LandingScreen.namedRoute: (context) => const LandingScreen(),
+        HomeScreen.namedRoute: (context) => const HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screen/landing_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  static const namedRoute = '/landingscreen';
+
+  //Navigator.of(context).pushNamed(LandingScreen.namedRoute),
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
